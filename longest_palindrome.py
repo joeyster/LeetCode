65d5d4328779a88fd5daf3a5867bc79055b010c9
Joey <30329601+joeyster@users.noreply.github.com>
2019-05-25 00:35:57 -0700

    Add files via upload

---
+++ longest_palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        dict = {}
+        for elem in range(len(s)):
+            temp = ""
+            for char in range(elem,len(s)):
+                temp = temp + s[char]
+                if temp not in dict:
+                    reverseTemp = temp[::-1]
+                    if reverseTemp == temp:
+                        dict[temp] = len(temp)
+        best = ""
+        for key in dict:
+            if len(key) > len(best):
+                best = key
+        return best
+            
