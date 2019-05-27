40a7040b79bb985f2ddf84bc98450f4a66599562
Joey <30329601+joeyster@users.noreply.github.com>
2019-05-26 22:28:53 -0700

    Add files via upload

---
+++ findMedianSortedArrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        #combine nums1, num2
+        temp = nums1 + nums2
+        temp.sort()
+        if len(temp) == 2:
+            return (temp[0]+temp[1])/2
+        elif len(temp) % 2 == 0:
+            first = len(temp) // 2
+            before = first - 1
+            return (temp[first]+temp[before]) / 2
+        else:
+            return temp[len(temp) // 2]
