be5199d723c7c8aa41737a65856b6a64ec767173
Joey <30329601+joeyster@users.noreply.github.com>
2019-05-27 23:22:29 -0700

    Add files via upload

---
+++ reverseInteger.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+        elif x < 0:
+            temp = str(x)
+            temp = temp.lstrip("-")
+            temp = temp[::-1]
+            temp = temp.lstrip("0")
+            temp = "-" + temp
+        else:
+            temp = str(x)
+            temp = temp[::-1]
+            temp = temp.lstrip("0")
+            
+        if int(temp) < -2**31 or int(temp) > 2**31-1:
+            return 0
+        else:
+            return int(temp)
+            
